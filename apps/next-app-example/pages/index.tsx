4f1b86190e2d70a877260dec0763086e030e223b
Yasniel <yasnielfajardoegues1408@gmail.com>
2022-07-05 00:51:50 -0400

    monorepo bases

---
+++ apps/next-app-example/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Index = () => {
+    return (
+        <div>
+            <button isSelected onClick={() => alert("Hello!")}>
+                Hello World!
+            </button>
+        </div>
+    );
+};
+export default Index;
---
+++ packages/example/src/index.tsx
@@ -0,0 +1,10 @@
+export const index = () => {
+    return (
+        <div>
+            <h1>Example</h1>
+            <p>This is an example component package!</p>
+        </div>
+    );
+};
+
+export default index;
